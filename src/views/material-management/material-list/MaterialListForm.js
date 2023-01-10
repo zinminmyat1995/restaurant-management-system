3c5fcc0f4176ea14d37ae4ec979b4252720d0730
Yuwa Ko Ko <yuwakoko9898@gmail.com>
2023-01-10 21:39:33 +0630

    form upgrade menu reg and material list

--- src/views/material-management/material-list/MaterialListForm.js
+++ src/views/material-management/material-list/MaterialListForm.js
@@ -1,132 +1,221 @@
 import React from "react";
 import {
-    CButton,
-    CCard,
-    CCardBody,
-    CCardHeader,
-    CCol,
-    CRow,
-    CLabel,
-    CInput,
-    CSelect
-} from '@coreui/react'
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CLabel,
+  CInput,
+  CSelect,
+  CImg,
+} from "@coreui/react";
 
-const MaterialListForm = (props)=>{
-    let {
-        handleChangeMaterialName,
-        materialName,
-        selectPurchaseType,
-        selectPurchaseValue,
-        selectPurChaseOnChange
-    } = props;
-    return(
-        <>
-            <CCard className="card-body">
-                <CCardHeader>
-                    <h5>Material List</h5>
-                </CCardHeader>
-                <CCardBody>
-                    <CRow>
-                        <CCol lg="2">
-                            <CLabel>
-                                Material Name
-                            </CLabel>
-                        </CCol>
-                        <CCol lg="4">
-                        <CInput
-                            autoFocus="focus"
-                            placeholder="Enter Material Name"
-                            type="text"
-                            value={materialName}
-                            onChange={(e) => handleChangeMaterialName(e)}
-                        />
-                        </CCol>
-                        <CCol lg="2">
-                            <CLabel>
-                                Purchase Type
-                            </CLabel>
-                        </CCol>
-                        <CCol lg="4">
-                            <CSelect
-                                value={selectPurchaseValue}
-                                onChange={selectPurChaseOnChange}
-                            >
-                            <option value="">---Select Purchase Type---</option>
-                                {selectPurchaseType.map((data, index) => {
-                                return (
-                                    <option key={index} id={data.purchase_type} value={data.purchase_type}>
-                                    {data.name}
-                                    </option>
-                                );
-                                })}
-                            </CSelect>
-                        </CCol>
-                    </CRow>
-                </CCardBody>
-            </CCard>
-            {/* {mainTable.length > 0 && ( */}
-            <CCard className="card-body">
-                <CCardBody>
-                <div className="list-bg">
-                <div className="list-heading mr-3">
-                    <h5 className="font-weight-bold responsive-list-heading-font">
-                    {/* <CImg
-                        className="list-img"
-                        src="/avatars/list.png"
-                        width={20}
-                        height={20}
-                    ></CImg> */}
-                    Material List Table
-                    </h5>
-                    <p className="font-weight-bold">
-                    {/* <span className="hideRow">Total Row:</span> {total} row(s) */}
-                    </p>
-                </div>
-                <div className="overflow-style">
-                    <table className="list-table">
-                    <thead className="text-center">
-                        <tr>
-                        <th style={{ width: 160 }}>No</th>
-                        <th style={{ width: 160 }}>Material ID</th>
-                        <th style={{ width: 160 }}>Material Name</th>
-                        <th style={{ width: 160 }}>Purchase Type</th>
-                        <th style={{ width: 480 }} colSpan="2">
-                            Action
+const MaterialListForm = (props) => {
+  let {
+    handleChangeMaterialName,
+    materialName,
+    selectPurchaseType,
+    selectPurchaseValue,
+    selectPurChaseOnChange,
+    tableData,
+    searchClick,
+    deleteClick,
+    editClick,
+  } = props;
+  return (
+    <>
+      <CCard>
+        <CCardHeader>
+          <h4>Material List</h4>
+        </CCardHeader>
+        <CCardBody>
+          <CRow>
+            <CCol lg="6">
+              <CRow>
+                <CCol lg="1"></CCol>
+                <CCol lg="4">
+                  <CLabel style={{ marginTop: "5px" }}> Material Name</CLabel>
+                </CCol>
+                <CCol lg="6">
+                  <CInput
+                    autoFocus="focus"
+                    placeholder="Enter Material Name"
+                    type="text"
+                    value={materialName}
+                    onChange={(e) => handleChangeMaterialName(e)}
+                  />
+                </CCol>
+                <CCol lg="1"></CCol>
+              </CRow>
+            </CCol>
+            <CCol lg="6">
+              <CRow>
+                <CCol lg="1"></CCol>
+                <CCol lg="4">
+                  <CLabel style={{ marginTop: "5px" }}> Purchase Type</CLabel>
+                </CCol>
+                <CCol lg="6">
+                  <CSelect
+                    value={selectPurchaseValue}
+                    onChange={selectPurChaseOnChange}
+                  >
+                    <option value="">---Select Purchase Type---</option>
+                    {selectPurchaseType.map((data, index) => {
+                      return (
+                        <option
+                          key={index}
+                          id={data.purchase_type}
+                          value={data.purchase_type}
+                        >
+                          {data.name}
+                        </option>
+                      );
+                    })}
+                  </CSelect>
+                </CCol>
+                <CCol lg="1"></CCol>
+              </CRow>
+            </CCol>
+          </CRow>
+
+          {/* {mainTable.length > 0 && ( */}
+          <CRow alignHorizontal="center" className="mt-5">
+            <CButton className="form-btn" onClick={searchClick}>
+              Search
+            </CButton>
+          </CRow>
+
+          <CRow style={{ overflow: "auto" }}>
+            <CCol lg="12" xs="12">
+              {tableData.length > 0 && (
+                <>
+                  <CRow className="mt-5">
+                    <CCol lg="10">
+                      <h4 style={{ marginLeft: "18px" }}>
+                        Material List Table
+                      </h4>
+                    </CCol>
+
+                    <CCol lg="2">
+                      <p style={{ paddingRight: "10px", textAlign: "end" }}>
+                        <span>Total Row:</span> {tableData.length} row(s)
+                      </p>
+                    </CCol>
+                  </CRow>
+                  <table
+                    className="table table-striped"
+                    style={{
+                      width: "99.5%",
+                      margin: "30px 4px",
+                      border: "1px solid",
+                      textAlign: "center",
+                    }}
+                  >
+                    <thead style={{ backgroundColor: "#9ca13c" }}>
+                      <tr>
+                        <th style={{ border: "1px solid" }} scope="col">
+                          <label style={{ color: "white" }}>No</label>
                         </th>
-                        </tr>
+                        <th style={{ border: "1px solid" }} scope="col">
+                          <label style={{ color: "white" }}>Material ID</label>
+                        </th>
+                        <th style={{ border: "1px solid" }} scope="col">
+                          <label style={{ color: "white" }}>
+                            Material Name
+                          </label>
+                        </th>
+                        <th style={{ border: "1px solid" }} scope="col">
+                          <label style={{ color: "white" }}>
+                            Purchase Type
+                          </label>
+                        </th>
+                        <th
+                          style={{ border: "1px solid" }}
+                          colSpan="2"
+                          scope="col"
+                        >
+                          <label style={{ color: "white" }}>Action</label>
+                        </th>
+                      </tr>
                     </thead>
-                    <tbody className="text-center">
-                        {/* {mainTable.length > 0 &&
-                            mainTable.map((data, index) => { */}
-                            {/* return ( */}
-                            <tr>
-                                {/* <td style={{ width: 160 }}>{index}</td>
-                                <td style={{ width: 160 }}>{material_id} </td>
-                                <td style={{ width: 160 }}>{material_name}</td>
-                                <td style={{ width: 160 }}>{purchase_type}</td>
-                                <td style={{ width: 120 }}>
-                                    Edit
-                                </td>
-                                <td style={{ width: 120 }}>
-                                    Delete
-                                </td> */}
-                                <td style={{ width: 160 }}>1</td>
-                                <td style={{ width: 160 }}>10001</td>
-                                <td style={{ width: 160 }}>Chicken</td>
-                                <td style={{ width: 160 }}>3</td>
-                                <td style={{ width: 120 }}>
-                                    Edit
-                                </td>
-                                <td style={{ width: 120 }}>
-                                    Delete
-                                </td>
-                            </tr>
-                            {/* ); */}
-                        {/* })} */}
+                    <tbody>
+                      {tableData.map((data, index) => {
+                        return (
+                          <tr key={index}>
+                            <td style={{ border: "1px solid" }}>{index + 1}</td>
+                            <td style={{ border: "1px solid" }}>
+                              {data.material_id}
+                            </td>
+                            <td style={{ border: "1px solid" }}>
+                              {data.material_name}
+                            </td>
+                            <td style={{ border: "1px solid" }}>
+                              {data.purchase_type}
+                            </td>
+
+                            <td style={{ border: "1px solid" }}>
+                              <div className="user-before">
+                                <CImg
+                                  src="/image/Edit-Component-inactive.svg"
+                                  onClick={() => {
+                                    editClick(data.material_id);
+                                  }}
+                                  style={{
+                                    width: "40px",
+                                    height: "40px",
+                                    cursor: "pointer",
+                                  }}
+                                ></CImg>
+                                <CImg
+                                  className="user-after"
+                                  src="/image/Edit-Component-active.svg"
+                                  onClick={() => {
+                                    editClick(data.material_id);
+                                  }}
+                                  style={{
+                                    width: "40px",
+                                    height: "40px",
+                                    cursor: "pointer",
+                                  }}
+                                ></CImg>
+                              </div>
+                            </td>
+
+                            <td style={{ border: "1px solid" }}>
+                              <div className="user-before">
+                                <CImg
+                                  src="/image/Delete-Component-inactive.svg"
+                                  onClick={() => deleteClick(data.material_id)}
+                                  style={{
+                                    width: "40px",
+                                    height: "40px",
+                                    cursor: "pointer",
+                                  }}
+                                ></CImg>
+                                <CImg
+                                  className="user-after"
+                                  src="/image/Delete-Component-active.svg"
+                                  onClick={() => deleteClick(data.material_id)}
+                                  style={{
+                                    width: "40px",
+                                    height: "40px",
+                                    cursor: "pointer",
+                                  }}
+                                ></CImg>
+                              </div>
+                            </td>
+                          </tr>
+                        );
+                      })}
                     </tbody>
-                    </table>
-                </div>
-                {/* {total > 20 && (
+                  </table>
+                </>
+              )}
+            </CCol>
+          </CRow>
+          {/* {total > 20 && (
                     <CRow alignHorizontal="center" className="mt-3">
                     <CPagination
                         activePage={currentPage}
@@ -140,12 +229,11 @@ const MaterialListForm = (props)=>{
                     ></CPagination>
                     </CRow>
                 )} */}
-                </div>
-            </CCardBody>
-            </CCard>
-          {/* )} */}
-        </>
-    )
-}
+        </CCardBody>
+      </CCard>
+      {/* )} */}
+    </>
+  );
+};
 
 export default MaterialListForm;
--- src/views/menu-management/menu-register/MenuRegisterIndex.js
+++ src/views/menu-management/menu-register/MenuRegisterIndex.js
@@ -1,29 +1,284 @@
-import React from 'react'
+import React, { useState } from "react";
 import {
   CButton,
   CCard,
   CCardBody,
   CCardHeader,
   CCol,
-  CRow
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-
+  CImg,
+  CInput,
+  CLabel,
+  CRow,
+  CSelect,
+} from "@coreui/react";
+import Modal from "./Modal";
+import Message from "../../common/SuccessError";
+import Loading from "../../common/Loading"
+import message from "../../common/CommonMessage"
+import $ from "jquery";
+import { display } from "@mui/system";
 const MenuRegisterIndex = () => {
+  const [modalShow, setModalShow] = useState(false); // for modal show or hide
+  const [title, setTitle] = useState(""); // for modal title
+  const [type, setType] = useState(""); // for plus or minus type
+  const [error, setError] = useState([]); // for error message
+  const [success, setSuccess] = useState([]); // for success message
+  const [loading, setLoading] = useState(false);  // for loading
+  const [modalSuccess, setModalSuccess] = useState([]);  // for modal success
+  const [modalError, setModalError] = useState([]);   // for modal error
+  const [plusNameCate, setPlusNameCate] = useState("");  // for plus modal name
+  const [plusNameType, setPlusNameType] = useState("");  // for plus modal name
+  const [minusName, setMinusName] = useState("");  // for minus modal name
+  const [minusNameData, setMinusNameData] = useState([]); // for minus modal name data
+  const [meatPriceData, setMeatPriceData] = useState([
+    { name: "Wat Thar", isChecked: "false" },
+    { name: "fish", isChecked: "false" }
+  ]); // for minus modal name data
+
+
+  // for plus and minus button click function
+  let btnClcik = (type) => {
+    setModalShow(true);
+    setType(type);
+
+    if (type == "plus1") {
+      setTitle("Add Menu Category")
+    }
+    if (type == "plus2") {
+      setTitle("Add Meat")
+    }
+    if (type == "plus") {
+      setTitle("Add Menu Type")
+    }
+    if (type == "minus1") {
+      setTitle("Remove Menu Category")
+    }
+    if (type == "minus") {
+      setTitle("Remove Menu Type")
+    }
+
+  };
+
+  // for close btn function
+  let cancelBtn = () => {
+    setModalShow(false); setModalError([]); setModalSuccess([]);
+  };
+
+  // for ok button function
+  let okBtn = () => {
+    let str = "";
+    if (type == "plus1" && plusNameCate == "") {
+      str = (message.JSE005).replace("%s", "Menu Category!")
+    }
+    if (type == "plus" && plusNameCate == "") {
+      str = (message.JSE005).replace("%s", "Menu Type!")
+    }
+    if (type == "minus1" && minusName == "") {
+      str = (message.JSE001).replace("%s", "Menu Category!")
+    }
+    if (type == "minus" && minusName == "") {
+      str = (message.JSE001).replace("%s", "Menu Type!")
+    }
+    if (str != "") {
+      setModalError([str]); setModalSuccess([]);
+      $("html, body").animate({ scrollTop: 0 }, 1000);
+    } else {
+      setModalError([]); setModalSuccess([]);
+
+    }
+
+  }
+
   return (
     <CRow>
       <CCol xs="12">
+        <Loading start={loading} />
+        <Message success={success} error={error} />
         <CCard>
           <CCardHeader>
-           MenuRegisterIndex
+            <h4>Menu Registration</h4>
           </CCardHeader>
           <CCardBody>
+            <CRow className="mt-3">
+              <CCol lg="2" style={{ textAlign: "end" }}>
+                <CLabel>Shop Name</CLabel>
+              </CCol>
+              <CCol lg="1"></CCol>
+              <CCol lg="4">
+                <CInput type="text" />
+              </CCol>
+            </CRow>
+            <CRow className="mt-3">
+              <CCol lg="2" style={{ textAlign: "end" }}>
+                <CLabel>Menu ID</CLabel>
+              </CCol>
+              <CCol lg="1"></CCol>
+              <CCol lg="4">
+                <CInput type="text" />
+              </CCol>
+            </CRow>
+            <CRow className="mt-4">
+              <CCol lg="2" style={{ textAlign: "end" }}>
+                <CLabel className="required">Menu Catagory</CLabel>
+              </CCol>
+              <CCol lg="1"></CCol>
+              <CCol lg="3">
+                <CSelect>
+                  <option key="" value="">
+                    ---Select---
+                  </option>
+                </CSelect>
+              </CCol>
+              <CCol lg="1" style={{ marginLeft: "-13px" }}>
+                <CRow alignHorizontal="start">
+                  <CCol>
+                    <CImg
+                      className="btn-hover"
+                      src={"/image/plus.svg"}
+                      width="33px"
+                      onClick={() => btnClcik("plus1")}
+                    />
+                  </CCol>
+                  <CCol>
+                    <CImg
+                      className="btn-hover"
+                      src={"/image/minus.svg"}
+                      width="33px"
+                      onClick={() => btnClcik("minus1")}
+                    />
+                  </CCol>
+                </CRow>
+              </CCol>
+            </CRow>
+            <CRow className="mt-4">
+              <CCol lg="2" style={{ textAlign: "end" }}>
+                <CLabel className="required">Menu Type</CLabel>
+              </CCol>
+              <CCol lg="1"></CCol>
+              <CCol lg="3">
+                <CSelect>
+                  <option key="" value="">
+                    ---Select---
+                  </option>
+                </CSelect>
+              </CCol>
+              <CCol lg="1" style={{ marginLeft: "-13px" }}>
+                <CRow alignHorizontal="start">
+                  <CCol>
+                    <CImg
+                      className="btn-hover"
+                      src={"/image/plus.svg"}
+                      width="33px"
+                      onClick={() => btnClcik("plus")}
+                    />
+                  </CCol>
+                  <CCol>
+                    <CImg
+                      className="btn-hover"
+                      src={"/image/minus.svg"}
+                      width="33px"
+                      onClick={() => btnClcik("minus")}
+                    />
+                  </CCol>
+                </CRow>
+              </CCol>
+            </CRow>
+            <CRow className="mt-3">
+              <CCol lg="2" style={{ textAlign: "end" }}>
+                <CLabel>Menu Name</CLabel>
+              </CCol>
+              <CCol lg="1"></CCol>
+              <CCol lg="4">
+                <CInput type="text" />
+              </CCol>
+            </CRow>
+
+
+            <Modal
+              show={modalShow}
+              title={title}
+              type={type}
+              cancelBtn={cancelBtn}
+              error={modalError}
+              success={modalSuccess}
+              okBtn={okBtn}
+              plusNameCate={plusNameCate}
+              plusNameChangeCate={(e) => setPlusNameCate(e.target.value)}
+              plusNameType={plusNameType}
+              plusNameChangeType={(e) => setPlusNameType(e.target.value)}
+
+
+
+            />
+
+
+
           </CCardBody>
+
+          <CRow>
+            <CCol lg="2"></CCol>
+            <CCol lg="8" style={{ border: "1px solid" }}>
+
+              <CRow className="mt-3">
+                <CCol lg="12" style={{ display: "flex" }}>
+                  <h4 style={{ marginTop: "6px" }}>Meat and Price</h4> &nbsp;&nbsp;&nbsp;
+
+                  <CImg
+                    className="btn-hover"
+                    src={"/image/plus.svg"}
+                    width="40px"
+
+                    onClick={() => btnClcik("plus2")}
+                  />
+
+                </CCol>
+               
+
+              </CRow>
+
+               {meatPriceData.length != 0 &&(
+
+meatPriceData.map((data, index) => {
+  return (
+    <table   className="table table-striped"
+    style={{
+      width: "99.5%",
+      margin: "30px 4px",
+     
+      textAlign: "center",
+    }}>
+    <tr key={index}>
+      <td style={{ border: "none" ,width:"30px" }}><CInput type="checkbox" style={{width:"20px"}} /></td>
+      <td style={{ border: "none" ,width:"200px"}}>{data.name}</td>
+      <td style={{ border: "none",width:"400px"}}><CInput type="text" /></td>
+      <td style={{ border: "none" ,width:"50px"}}>Kyat</td>
+      <td style={{ border: "none" ,width:"50px"}}>   <CImg
+                      className="btn-hover"
+                      src={"/image/minus.svg"}
+                      width="33px"
+                      onClick={() => btnClcik("minus2")}
+                    /></td>
+      
+      
+    </tr>
+    </table>
+
+  );
+})
+
+               )}  
+            
+            </CCol>
+            <CCol lg="2"></CCol>
+          </CRow>
+          <CRow alignHorizontal="center" className="mt-5">
+            <CButton className="form-btn">Save</CButton>
+          </CRow>
         </CCard>
+
       </CCol>
-
     </CRow>
-  )
-}
+  );
+};
 
-export default MenuRegisterIndex
+export default MenuRegisterIndex;

505c7baebb2bdc3be3eac305c62c35544ef9be78
phyoe <phyoewaiaung082@gmail.com>
2023-01-15 00:48:53 +0630

    import material list form

--- src/views/employee-management/employee-list/EmployeeListForm.js
+++ src/views/employee-management/employee-list/EmployeeListForm.js
@@ -1,15 +1,17 @@
 import { CButton, CCol, CLabel, CRow, CSelect } from '@coreui/react'
-import React from 'react'
+import React from 'react';
+import NPagination from '../../common/pagination/NPagination';
 
 function EmployeeListForm(props) {
-  let {shopName,selectChange,searchClick} = props;
+  let {shopName,selectChange,searchClick,employeeList,totalRow,currentPage,lastPage,shopCode,pagination,
+      editClick, delClick} = props;
   return (
     <div className='mt-4'>
       <CRow alignHorizontal='center' className="mb-3">
         <CCol lg="4">
           <CRow className="align-items-center">
             <CCol lg="4">
-                <CLabel>Shop Name</CLabel>
+                <CLabel style={{display:'contents'}}>Shop Name</CLabel>
             </CCol>
             <CCol lg="8">
                 <CSelect className="cursor-style" value={shopName} onChange={selectChange}>
@@ -28,28 +30,57 @@ function EmployeeListForm(props) {
       </CRow>
       <CRow className='mt-5'>
         <CCol>
-          <div className='table-responsive'>
-            <table className='table emp-list-table'>
+          <p className='mb-0 font-weight-bold'>Total : 15 row(s)</p>
+          <div className='overflow'>
+            <table className='emp-list-table'>
               <thead>
                 <tr>
-                  <th width={50}>No</th>
+                  <th width={50} className="text-center">No</th>
                   <th width={120}>Shop Code</th>
                   <th width={120}>Employee ID</th>
                   <th width={230}>Employee Name</th>
-                  <th width={200}>Date Of Birth</th>
-                  <th width={120}>Gender</th>
-                  <th width={200}>Phone No</th>
-                  <th width={120}>Address</th>
+                  <th className='text-center' width={150}>Date Of Birth</th>
+                  <th className='text-center' width={120}>Gender</th>
+                  <th width={150}>Phone No</th>
+                  <th width={220}>Address</th>
                   <th width={120}>Role</th>
-                  <th width={300}>Action</th>
+                  <th className='text-center' width={200}>Action</th>
                 </tr>
               </thead>
               <tbody>
+                {employeeList.map((item,index)=> {
+                  return(
+                    <tr key={index}>
+                    <td width={50} className="text-center">{index + 1}</td>
+                    <td width={120}>{item.shop_code}</td>
+                    <td width={120}> {item.emp_id}</td>
+                    <td width={230}>{item.emp_name}</td>
+                    <td className='text-center' width={150}>{item.dob}</td>
+                    <td className='text-center' width={120}>{item.gender}</td>
+                    <td width={150}>{item.phone}</td>
+                    <td width={220}>{item.address}</td>
+                    <td width={120}>{item.role}</td>
+                    <td width={200} className='text-center'>
+                      <label onClick={()=>editClick(item.id)} className='emp-table-btn btn-edit' style={{marginRight:'26px',width:'25%'}}>Edit</label>
+                      <label onClick={()=>delClick(item.id)} className='emp-table-btn btn-del' style={{width:'25%'}}>Delete</label>
+                    </td>
+                    </tr>
+                  )
+                })}
               </tbody>
             </table>
           </div>
         </CCol>
       </CRow>
+      {totalRow > 10 &&
+      <NPagination
+            activePage = {currentPage}
+            pages = {lastPage}
+            currentPage = {currentPage}
+            totalPage = {lastPage}
+            pagination = {pagination}
+            />
+      }
     </div>
   )
 }
